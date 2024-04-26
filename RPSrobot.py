ecbed221dbcc9d943a7421c81b0d4702f4a71cce
Hadley <hadley bell>
2024-04-25 18:02:26 -0600

    additions to motor file for buttons and LED

---
+++ RPSrobot.py
@@ -0,0 +1,20 @@
+# While stop button is not pressed 
+    # When start button is pressed
+        # start videocapture
+        # put arm in "home position"
+        # LCD display "lets play"
+        # LEDs 3, 2, 1, go
+        
+        # user gives signal
+        # robot also throws signal randomly
+        # score is calculated 
+        # score displayed "Bot:1 Human:0"
+        
+     # When re-start button is pressed
+        # reset score
+        # keep playing
+        
+        
+        
+    
+    
